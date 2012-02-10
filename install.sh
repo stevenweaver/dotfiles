c7cd11a173b5d1349d44a890c6b623096a2f84e0
Steven Weaver <weaverst@gmail.com>
2012-02-10 11:21:09 -0800

    install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+git submodule init
+git submodule update
+ln -sf ~/.dot_files/.bashrc ~/.bashrc
+ln -sf ~/.dot_files/.vimrc ~/.vimrc
+ln -sf ~/.dot_files/.vim ~/.vim
+ln -sf ~/.dot_files/.tmux.conf ~/.tmux.conf
