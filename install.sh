d00655d9dc035d0c9cda7e023aed2200e78b9e5c
sweaver <sweaver@probiscis.ayeaye.ucsd.edu>
2012-06-14 10:42:19 -0700

    adding zsh to install

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ ln -sf ~/.dot_files/.bashrc ~/.bashrc
 ln -sf ~/.dot_files/.vimrc ~/.vimrc
 ln -sf ~/.dot_files/.vim ~/.vim
 ln -sf ~/.dot_files/.tmux.conf ~/.tmux.conf
+ln -sf ~/.dot_files/.zshrc ~/.zshrc
 /bin/bash
