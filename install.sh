9b591d2132ed4fd0d7b91f0971c95d9c34b049d2
Steven Weaver <sweaver@temple.edu>
2019-12-21 11:19:06 -0700

    updating gitmoudles:

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 git submodule init && git submodule update
-virtualenv ~/.neovimpython
-npm install -g tern
+#virtualenv ~/.neovimpython
 rm ~/.config; ln -sf ~/.dotfiles/.config ~/.config
 rm ~/.bashrc; ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 rm ~/.vimrc; ln -sf ~/.dotfiles/.vimrc ~/.vimrc
@@ -8,7 +7,7 @@ rm ~/.vim; ln -sf ~/.dotfiles/.vim ~/.vim
 rm ~/.tmux.conf; ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
 rm ~/.zshrc; ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 rm ~/.gitconfig; ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
-rm ~/.oh-my-zsh; ln -sf ~/.dotfiles/oh-my-zsh ~/.oh-my-zsh
+rm ~/.oh-my-zsh; ln -sf ~/.dotfiles/ohmyzsh ~/.oh-my-zsh
 rm -rf ~/.newsbeuter; ln -sf ~/.dotfiles/.newsbeuter ~/.newsbeuter
 rm -rf ~/.jshintrc; ln -sf ~/.dotfiles/jshintrc ~/.jshintrc
 rm -rf ~/.tern-project; ln -sf ~/.dotfiles/.tern-project ~/.tern-project
